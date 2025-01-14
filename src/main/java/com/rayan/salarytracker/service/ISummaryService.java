658a182f49fe2b271c188c4a965dcec05ec0a8c0
rharbi <iirharbi@gmail.com>
2025-01-14 19:10:14 +0300

    Feat: Introduce SummaryService interface

---
+++ src/main/java/com/rayan/salarytracker/service/ISummaryService.java
@@ -0,0 +1,7 @@
+package com.rayan.salarytracker.service;
+
+import com.rayan.salarytracker.dao.IGenericCRUD;
+import com.rayan.salarytracker.model.Summary;
+
+public interface ISummaryService extends IGenericCRUD<Summary> {
+}
