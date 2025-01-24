b72adbcb20f0d19ab56e644b68aa81b8bebfa63f
rharbi <iirharbi@gmail.com>
2025-01-24 20:54:12 +0300

    Refactor: Enhance Error handling when user not authorized

--- src/main/java/com/rayan/salarytracker/filters/JwtAuthenticationFilter.java
+++ src/main/java/com/rayan/salarytracker/filters/JwtAuthenticationFilter.java
@@ -8,17 +8,16 @@ import jakarta.annotation.Priority;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.NotAuthorizedException;
 import jakarta.ws.rs.Priorities;
+import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
-import jakarta.ws.rs.core.Context;
-import jakarta.ws.rs.core.HttpHeaders;
-import jakarta.ws.rs.core.SecurityContext;
-import jakarta.ws.rs.core.UriInfo;
+import jakarta.ws.rs.core.*;
 import jakarta.ws.rs.ext.Provider;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Set;
 
 @Provider
@@ -81,14 +80,16 @@ public class JwtAuthenticationFilter implements ContainerRequestFilter {
                 if (user != null && jwtService.isTokenValid(token, user)) {
                     containerRequestContext.setSecurityContext(new CustomSecurityContext(user));
                     return;
-                } else {
-                    LOGGER.error("Invalid token.");
                 }
             }
 
         } catch (Exception e) {
-            LOGGER.error("Invalid token.");
-            throw new NotAuthorizedException("Invalid token.");
+            LOGGER.error("Authorization Header does not exist, or is invalid.");
+            throw new WebApplicationException(Response.status(Response.Status.UNAUTHORIZED)
+                    .header(HttpHeaders.WWW_AUTHENTICATE, "Bearer realm=\"Restricted Area\"")
+                    .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                    .entity(Collections.singletonMap("error", "Authorization header is missing or invalid."))
+                    .build());
         }
     }
 
