c654fdc8e31cd3e1b6320ae48b628fe790bfc4f9
rharbi <iirharbi@gmail.com>
2025-02-13 12:29:48 +0300

    Introduce SalaryTestDTO for IT test

---
+++ src/main/java/com/rayan/salarytracker/dto/salary/SalaryTestDTO.java
@@ -0,0 +1,14 @@
+package com.rayan.salarytracker.dto.salary;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class SalaryTestDTO {
+    private Long id;
+}
