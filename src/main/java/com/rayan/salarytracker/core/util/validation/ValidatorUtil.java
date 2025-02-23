7a3a680294546820766b3ccb355b54ae691a7603
rharbi <iirharbi@gmail.com>
2025-02-23 19:10:52 +0300

    Make ValidatorUtil managed by CDI

--- src/main/java/com/rayan/salarytracker/core/util/validation/ValidatorUtil.java
+++ src/main/java/com/rayan/salarytracker/core/util/validation/ValidatorUtil.java
@@ -3,30 +3,27 @@ package com.rayan.salarytracker.core.util.validation;
 
 import com.rayan.salarytracker.core.exception.EntityAlreadyExistsException;
 import com.rayan.salarytracker.core.exception.EntityInvalidArgumentsException;
-import com.rayan.salarytracker.dao.IUserDAO;
-import com.rayan.salarytracker.dao.impl.UserDAO;
 import com.rayan.salarytracker.dto.user.UserInsertDTO;
-import com.rayan.salarytracker.mapper.Mapper;
 import com.rayan.salarytracker.service.IUserService;
-import com.rayan.salarytracker.service.impl.UserService;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+@ApplicationScoped
 public class ValidatorUtil {
 
     private static final Logger LOGGER = LogManager.getLogger(ValidatorUtil.class.getName());
 
+    @Inject
+    IUserService userService;
 
-    private static final IUserDAO userDAO = new UserDAO();
-    private static final Mapper mapper = new Mapper();
-    private static final IUserService userService = new UserService(userDAO, mapper);
-
-
-    public static void validateDTO(UserInsertDTO userInsertDTO) throws EntityAlreadyExistsException, EntityInvalidArgumentsException {
-        // Logical Validation
+    public void validateDTO(UserInsertDTO userInsertDTO) throws EntityAlreadyExistsException, EntityInvalidArgumentsException {
         if (userService.isEmailExists(userInsertDTO.getEmail())) {
+            LOGGER.error("Email {} already exists", userInsertDTO.getEmail());
             throw new EntityAlreadyExistsException("User ", "User with Email: " + userInsertDTO.getEmail() + " is Already exist");
         } else if (!userInsertDTO.getPassword().equals(userInsertDTO.getConfirmPassword())) {
+            LOGGER.error("Password does not match");
             throw new EntityInvalidArgumentsException("User ", "Password and confirm Password do not match!");
         }
     }
