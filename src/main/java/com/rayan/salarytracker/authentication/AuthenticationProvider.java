97107cfd632dd49aa29d5f59dec1beff17a4c8f5
rharbi <iirharbi@gmail.com>
2025-03-05 06:32:45 +0300

    Use filed injection

--- src/main/java/com/rayan/salarytracker/authentication/AuthenticationProvider.java
+++ src/main/java/com/rayan/salarytracker/authentication/AuthenticationProvider.java
@@ -9,16 +9,8 @@ import jakarta.inject.Inject;
 @ApplicationScoped
 public class AuthenticationProvider {
 
-    private IUserService userService;
-
-    public AuthenticationProvider() {
-
-    }
-
     @Inject
-    public AuthenticationProvider(IUserService userService) {
-        this.userService = userService;
-    }
+    private IUserService userService;
 
     public boolean authenticate(UserLoginDTO userLoginDTO) throws EntityNotFoundException {
         return userService.isUserValid(userLoginDTO.getEmail(), userLoginDTO.getPassword());
